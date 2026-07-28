4f701bdfa7b83e4d122f5c070ed4c21f40014c47
Code-2-Do <shukla.pranjal969@gmail.com>
2026-07-28 15:21:03 +0530

    Updated weather app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <div>
+            <img src="src/components/Copilot_20260726_211841.png"/>
+            <button>About Me</button>
+        </div>
+    )
+}
+
+export default Navbar;
