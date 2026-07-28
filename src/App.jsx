8ccc437f89ac8fdafa564ea0213e612004699032
Code-2-Do <shukla.pranjal969@gmail.com>
2026-07-28 23:34:02 +0530

    Backgroundimage sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React,{ useState } from "react";
 import axios from "axios";
 import Navbar from "./components/Navbar";
-import background from "./components/23643966790090249.jpg";
 
 export default function App() {
   const [weather, setWeather] = useState(null);
@@ -86,12 +85,7 @@ export default function App() {
   
 
   return (
-    <div className="bruhhhhhhh"
-     style={{
-      backgroundImage: `url(${background})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-     }}>
+    <div className="bruhhhhhhh">
       <div className="Navy">
         <Navbar className="Vadu"/>
       </div>
