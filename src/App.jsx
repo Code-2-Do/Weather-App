ad26a9e220c8d51680e87d0e58c5341f3c01d6c8
Code-2-Do <shukla.pranjal969@gmail.com>
2026-07-28 17:47:58 +0530

    Reasponsive change

--- src/App.jsx
+++ src/App.jsx
@@ -138,7 +138,7 @@ export default function App() {
 
           </div>
         </div>
-        <div className="wrap">
+        <div className="wrap tur">
           {weatherData && (
             <div>
               <div className="flex items-center gap-2">
